6dcb1171d64d02082e0136a3ce678058cf855862
javahikers <472407594.qq.com>
2019-08-19 07:42:14 +0800

    '代码整理'

--- MyBatis_01_HelloWorld/src/com/atguigu/mybatis/dao/EmployeeMapper.java
+++ MyBatis_01_HelloWorld/src/com/atguigu/mybatis/dao/EmployeeMapper.java
@@ -3,7 +3,6 @@ package com.atguigu.mybatis.dao;
 import com.atguigu.mybatis.bean.Employee;
 
 public interface EmployeeMapper {
-	
+	public Employee selectEmp(Integer id);
 	public Employee getEmpById(Integer id);
-
 }
