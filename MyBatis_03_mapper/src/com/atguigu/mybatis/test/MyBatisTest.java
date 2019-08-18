af63bb10813b05e601cb2a0b8489746c18e2ebc1
javahikers <472407594.qq.com>
2019-08-19 07:43:16 +0800

    '代码整理'

--- MyBatis_03_mapper/src/com/atguigu/mybatis/test/MyBatisTest.java
+++ MyBatis_03_mapper/src/com/atguigu/mybatis/test/MyBatisTest.java
@@ -2,10 +2,7 @@ package com.atguigu.mybatis.test;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -20,27 +17,8 @@ import com.atguigu.mybatis.dao.EmployeeMapper;
 import com.atguigu.mybatis.dao.EmployeeMapperAnnotation;
 import com.atguigu.mybatis.dao.EmployeeMapperPlus;
 
-/**
- * 1、接口式编程
- * 	原生：		Dao		====>  DaoImpl
- * 	mybatis：	Mapper	====>  xxMapper.xml
- * 
- * 2、SqlSession代表和数据库的一次会话；用完必须关闭；
- * 3、SqlSession和connection一样她都是非线程安全。每次使用都应该去获取新的对象。
- * 4、mapper接口没有实现类，但是mybatis会为这个接口生成一个代理对象。
- * 		（将接口和xml进行绑定）
- * 		EmployeeMapper empMapper =	sqlSession.getMapper(EmployeeMapper.class);
- * 5、两个重要的配置文件：
- * 		mybatis的全局配置文件：包含数据库连接池信息，事务管理器信息等...系统运行环境信息
- * 		sql映射文件：保存了每一个sql语句的映射信息：
- * 					将sql抽取出来。	
- * 
- * 
- * @author lfy
- *
- */
+
 public class MyBatisTest {
-	
 
 	public SqlSessionFactory getSqlSessionFactory() throws IOException {
 		String resource = "mybatis-config.xml";
@@ -48,46 +26,33 @@ public class MyBatisTest {
 		return new SqlSessionFactoryBuilder().build(inputStream);
 	}
 
-	/**
-	 * 1、根据xml配置文件（全局配置文件）创建一个SqlSessionFactory对象 有数据源一些运行环境信息
-	 * 2、sql映射文件；配置了每一个sql，以及sql的封装规则等。 
-	 * 3、将sql映射文件注册在全局配置文件中
-	 * 4、写代码：
-	 * 		1）、根据全局配置文件得到SqlSessionFactory；
-	 * 		2）、使用sqlSession工厂，获取到sqlSession对象使用他来执行增删改查
-	 * 			一个sqlSession就是代表和数据库的一次会话，用完关闭
-	 * 		3）、使用sql的唯一标志来告诉MyBatis执行哪个sql。sql都是保存在sql映射文件中的。
-	 * 
-	 * @throws IOException
-	 */
 	@Test
-	public void test() throws IOException {
-
-		// 2、获取sqlSession实例，能直接执行已经映射的sql语句
-		// sql的唯一标识：statement Unique identifier matching the statement to use.
-		// 执行sql要用的参数：parameter A parameter object to pass to the statement.
+	public void test01() throws IOException {
+/**
+ 1，根据xml配置文件（全局配置文件）得到SqlSessionFactory对象；
+ 2，使用SqlSessionFactory，得到sqlSession对象。能直接执行已经映射的sql语句
+ 用它来执行增删改查 。一个sqlSession就是代表和数据库的一次会话，用完关闭。SqlSession和connection一样她都是非线程安全。每次使用都应该去获取新的对象。
+ 3，使用sql的唯一标志来告诉MyBatis执行哪个sql。sql都是保存在sql映射文件中的。
+ **/
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
-
 		SqlSession openSession = sqlSessionFactory.openSession();
 		try {
 			Employee employee = openSession.selectOne(
-					"com.atguigu.mybatis.EmployeeMapper.selectEmp", 1);
+					"com.atguigu.mybatis.dao.EmployeeMapper.selectEmp", 1);
 			System.out.println(employee);
 		} finally {
 			openSession.close();
 		}
-
 	}
 
 	@Test
-	public void test01() throws IOException {
-		// 1、获取sqlSessionFactory对象
+	public void test02() throws IOException {
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
-		// 2、获取sqlSession对象
 		SqlSession openSession = sqlSessionFactory.openSession();
 		try {
-			// 3、获取接口的实现类对象
-			//会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+/**
+ 3，获取接口的实现类对象。会为接口自动的创建一个代理对象，代理对象去执行增删改查方法
+ **/
 			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
 			Employee employee = mapper.getEmpById(1);
 			System.out.println(mapper.getClass());
@@ -99,7 +64,7 @@ public class MyBatisTest {
 	}
 	
 	@Test
-	public void test02() throws IOException{
+	public void test03() throws IOException{
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 		SqlSession openSession = sqlSessionFactory.openSession();
 		try{
@@ -112,78 +77,172 @@ public class MyBatisTest {
 	}
 	
 	/**
-	 * 测试增删改
-	 * 1、mybatis允许增删改直接定义以下类型返回值
-	 * 		Integer、Long、Boolean、void
-	 * 2、我们需要手动提交数据
-	 * 		sqlSessionFactory.openSession();===》手动提交
-	 * 		sqlSessionFactory.openSession(true);===》自动提交
-	 * @throws IOException 
+	 增删改只支持以下返回值类型,并且可定义以下任意一种返回值类型：
+	 		Integer、Long、Boolean、void
+	 提交数据的方式
+	 	1,手动提交==》sqlSessionFactory.openSession();
+	 	2,自动提交==》sqlSessionFactory.openSession(true);
 	 */
 	@Test
-	public void test03() throws IOException{
+	public void test04() throws IOException{
 		
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
-		//1、获取到的SqlSession不会自动提交数据
+		//这种方式获取SqlSession，需要手动提交数据
 		SqlSession openSession = sqlSessionFactory.openSession();
 		
 		try{
 			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
 			//测试添加
-			Employee employee = new Employee(null, "jerry4",null, "1");
-			mapper.addEmp(employee);
+			Employee employee = new Employee(null, "jerry4","javahiker@126.com", "1");
+			Long res = mapper.addEmp(employee);
 			System.out.println(employee.getId());
-			
+			System.out.println(res);
 			//测试修改
-			//Employee employee = new Employee(1, "Tom", "jerry@atguigu.com", "0");
-			//boolean updateEmp = mapper.updateEmp(employee);
-			//System.out.println(updateEmp);
+/*			Employee employee = new Employee(3, "Tom", "jerry@atguigu.com", "0");
+			boolean updateEmp = mapper.updateEmp(employee);
+			System.out.println(updateEmp);*/
+
 			//测试删除
-			//mapper.deleteEmpById(2);
-			//2、手动提交数据
+/*			mapper.deleteEmpById(7);*/
+			//手动提交数据
 			openSession.commit();
 		}finally{
 			openSession.close();
 		}
 		
 	}
-	
-	
+
+	/**
+	 * 多个参数
+	 */
 	@Test
-	public void test04() throws IOException{
-		
+	public void test05() throws IOException{
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
-		//1、获取到的SqlSession不会自动提交数据
 		SqlSession openSession = sqlSessionFactory.openSession();
-		
 		try{
 			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
-			//Employee employee = mapper.getEmpByIdAndLastName(1, "tom");
+			Employee employee = mapper.getEmpByIdAndLastNameDefault(1, "javahiker");
+			System.out.println(employee);
+		}finally{
+			openSession.close();
+		}
+	}
+
+	/**
+	 * 参数传map
+	 */
+	@Test
+	public void test06_02() throws IOException{
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		SqlSession openSession = sqlSessionFactory.openSession();
+		try{
+			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+			Map<String, Object> map = new HashMap<>();
+			map.put("id", 3);
+			map.put("lastName", "Tom");
+			map.put("tableName", "tbl_employee");
+			Employee employee = mapper.getEmpByMap(map);
+			System.out.println(employee);
+		}finally{
+			openSession.close();
+		}
+	}
+
+	/**
+	 * 命名参数
+	 */
+	@Test
+	public void test06_01() throws IOException{
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		SqlSession openSession = sqlSessionFactory.openSession();
+		try{
+			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+			Employee employee = mapper.getEmpByIdAndLastName(1,"javahiker");
+			System.out.println(employee);
+		}finally{
+			openSession.close();
+		}
+	}
+
+	/**
+	 * POJO
+	 */
+	@Test
+	public void test06_03() throws IOException{
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		SqlSession openSession = sqlSessionFactory.openSession();
+		try{
+			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+			Employee employeeParam = new Employee();
+			employeeParam.setId(1);
+			employeeParam.setLastName("javahiker");
+			Employee employee = mapper.getEmpByPojo(employeeParam);
+			System.out.println(employee);
+		}finally{
+			openSession.close();
+		}
+	}
+
+	/**
+	 Collection  List  数组
+ 	 key值：Collection==> collection/list
+	 		List==>collection/list
+	 		Array==>array
+	 */
+	@Test
+	public void test06_04() throws IOException{
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		SqlSession openSession = sqlSessionFactory.openSession();
+		try{
+			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+			Collection collection = new ArrayList();
+			collection.add(1);
+			collection.add(3);
+			List<Integer> list = new ArrayList<>();
+			list.add(1);
+			list.add(3);
+			Set<Integer>set = new HashSet<>();
+			set.add(1);
+			set.add(3);
+			Integer [] array = new Integer[]{1,3};
+			Employee employee = mapper.getEmpByList(list);
+			System.out.println(employee);
+		}finally{
+			openSession.close();
+		}
+	}
+
+	@Test
+	public void test06() throws IOException{
+		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+		SqlSession openSession = sqlSessionFactory.openSession();
+		try{
+			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
 			Map<String, Object> map = new HashMap<>();
 			map.put("id", 2);
 			map.put("lastName", "Tom");
 			map.put("tableName", "tbl_employee");
 			Employee employee = mapper.getEmpByMap(map);
-			
+
 			System.out.println(employee);
-			
+
 			/*List<Employee> like = mapper.getEmpsByLastNameLike("%e%");
 			for (Employee employee : like) {
 				System.out.println(employee);
 			}*/
-			
+
 			/*Map<String, Object> map = mapper.getEmpByIdReturnMap(1);
 			System.out.println(map);*/
 			/*Map<String, Employee> map = mapper.getEmpByLastNameLikeReturnMap("%r%");
 			System.out.println(map);*/
-			
+
 		}finally{
 			openSession.close();
 		}
 	}
-	
+
 	@Test
-	public void test05() throws IOException{
+	public void test07() throws IOException{
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 		SqlSession openSession = sqlSessionFactory.openSession();
 		try{
@@ -205,7 +264,7 @@ public class MyBatisTest {
 	}
 	
 	@Test
-	public void test06() throws IOException{
+	public void test08() throws IOException{
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 		SqlSession openSession = sqlSessionFactory.openSession();
 		
