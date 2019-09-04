2da64461323df964af5dfa06316a44104cf431c1
javahikers <472407594.qq.com>
2019-09-04 23:13:28 +0800

    '引入ehcache'

--- MyBatis_06_ssm/WebContent/index.jsp
+++ MyBatis_06_ssm/WebContent/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-<a href="getemps">查询所有员工</a>
+<a href="/employee/getemps">查询所有员工</a>
 </body>
 </html>
--- MyBatis_06_ssm/src/com/atguigu/mybatis/controller/EmployeeController.java
+++ MyBatis_06_ssm/src/com/atguigu/mybatis/controller/EmployeeController.java
@@ -9,18 +9,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.atguigu.mybatis.bean.Employee;
 import com.atguigu.mybatis.service.EmployeeService;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/employee")
 public class EmployeeController {
-	
+
 	@Autowired
 	EmployeeService employeeService;
 	
 	@RequestMapping("/getemps")
 	public String emps(Map<String,Object> map){
 		List<Employee> emps = employeeService.getEmps();
-		map.put("allEmps", emps);
+		map.put("allEmps", emps.subList(0,1));
 		return "list";
+	}
+
+	@RequestMapping(value = "/getempsCache", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Employee> cashTestm(){
+		List<Employee> emps = employeeService.getEmps();
+		return emps;
 	}
 
 }
