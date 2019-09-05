3744e16826606961b02e3c91b4391f8da076de06
javahikers <472407594.qq.com>
2019-09-06 06:27:35 +0800

    'mbg'

--- MyBatis_07_mbg/src/com/atguigu/mybatis/test/MyBatisTest.java
+++ MyBatis_07_mbg/src/com/atguigu/mybatis/test/MyBatisTest.java
@@ -21,8 +21,6 @@ import com.atguigu.mybatis.bean.EmployeeExample;
 import com.atguigu.mybatis.bean.EmployeeExample.Criteria;
 import com.atguigu.mybatis.dao.EmployeeMapper;
 
-/*import com.atguigu.mybatis.bean.Employee;
-import com.atguigu.mybatis.dao.EmployeeMapper;*/
 
 public class MyBatisTest {
 
@@ -44,13 +42,15 @@ public class MyBatisTest {
 				callback, warnings);
 		myBatisGenerator.generate(null);
 	}
-	
+
+
 	@Test
-	public void testMyBatis3Simple() throws IOException{
+	public void testMyBatis3_01() throws IOException{
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 		SqlSession openSession = sqlSessionFactory.openSession();
 		try{
 			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
+			//1，查询所有
 			List<Employee> list = mapper.selectByExample(null);
 			for (Employee employee : list) {
 				System.out.println(employee.getId());
@@ -59,22 +59,20 @@ public class MyBatisTest {
 			openSession.close();
 		}
 	}
-	
+
 	@Test
-	public void testMyBatis3() throws IOException{
+	public void testMyBatis3_02() throws IOException{
 		SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 		SqlSession openSession = sqlSessionFactory.openSession();
 		try{
 			EmployeeMapper mapper = openSession.getMapper(EmployeeMapper.class);
-			//xxxExample就是封装查询条件的
-			//1、查询所有
-			//List<Employee> emps = mapper.selectByExample(null);
-			//2、查询员工名字中有e字母的，和员工性别是1的
-			//封装员工查询条件的example
+			//2、查询员工名字中有e字母的，和员工性别是1的。xxExample就是封装查询条件的
 			EmployeeExample example = new EmployeeExample();
+	/*
+			SELECT id, last_name, email, gender, d_id FROM tbl_employee
+			WHERE ( last_name LIKE '%e%' AND gender = 1 ) OR email LIKE "%e%"
+	*/
 			//创建一个Criteria，这个Criteria就是拼装查询条件
-			//select id, last_name, email, gender, d_id from tbl_employee 
-			//WHERE ( last_name like ? and gender = ? ) or email like "%e%"
 			Criteria criteria = example.createCriteria();
 			criteria.andLastNameLike("%e%");
 			criteria.andGenderEqualTo("1");
@@ -82,12 +80,10 @@ public class MyBatisTest {
 			Criteria criteria2 = example.createCriteria();
 			criteria2.andEmailLike("%e%");
 			example.or(criteria2);
-			
 			List<Employee> list = mapper.selectByExample(example);
 			for (Employee employee : list) {
 				System.out.println(employee.getId());
 			}
-			
 		}finally{
 			openSession.close();
 		}
